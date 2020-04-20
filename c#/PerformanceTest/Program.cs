e252adb85290016552aa9524c324eaf63f83445c
Sebastian Maciejewski <sebastian.maciejewski@br-ag.eu>
2020-04-20 12:24:29 +0200

    Gitignore, C# TransformBlock test

---
+++ c#/PerformanceTest/Program.cs
@@ -0,0 +1,75 @@
+﻿using System.Text;
+using System.IO.Compression;
+using System.IO;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Threading.Tasks.Dataflow;
+
+namespace PerformanceTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var testFiles = Directory.GetFiles("../../test_data");
+
+            var downloadBlock = new TransformBlock<string, MemoryStream>(url =>
+            {
+                // Download file (async) and pass FileStream to unzipBlock
+                MemoryStream memoryStream = new MemoryStream();
+
+                using (FileStream fileStream = File.OpenRead(url))
+                {
+                    memoryStream.SetLength(fileStream.Length);
+                    fileStream.Read(memoryStream.GetBuffer(), 0, (int)fileStream.Length);
+                }
+
+                return memoryStream;
+            },
+            new ExecutionDataflowBlockOptions
+            {
+                MaxDegreeOfParallelism = 2, // How many files do we want to download at once? How good is download speed / bandwith?
+            });
+
+            var unzipBlock = new TransformBlock<MemoryStream, MemoryStream>(async memoryStream =>
+            {
+                ZipArchive zipArchive = new ZipArchive(memoryStream);
+                MemoryStream outputStream = new MemoryStream();
+
+                await Task.Run(() =>
+                {
+                    foreach (ZipArchiveEntry entry in zipArchive.Entries)
+                    {
+						// Load results to database
+                        entry.Open().CopyTo(outputStream);
+                        // Console.WriteLine(Encoding.ASCII.GetString(outputStream.ToArray()));
+                    }
+                });
+                return outputStream;
+            },
+            new ExecutionDataflowBlockOptions
+            {
+                BoundedCapacity = Environment.ProcessorCount * 3,
+                MaxDegreeOfParallelism = Environment.ProcessorCount,
+            });
+
+            downloadBlock.LinkTo(
+            unzipBlock,
+            new DataflowLinkOptions
+            {
+                PropagateCompletion = true,
+            });
+
+
+            foreach (var url in testFiles)
+            {
+                downloadBlock.Post(url);
+            }
+            downloadBlock.Complete();
+            unzipBlock.Completion.GetAwaiter().GetResult();
+
+            System.Environment.Exit(0);
+        }
+    }
+}
--- python/main.py
+++ python/main.py
@@ -2,18 +2,20 @@ from io import BytesIO
 from zipfile import ZipFile
 from urllib.request import urlopen
 import time
+import glob
 
-
+test_files = glob.glob('../test_data/*.zip')
 start_time = time.time()
+
 # res = urlopen("https://www.kaggle.com/tunguz/covid19-genomes/download")
 # zipfile = ZipFile(BytesIO(res.read()))
 
-file = open('test.zip', 'rb')
-zipfile = ZipFile(BytesIO(file.read()))
-print(zipfile.namelist())
-
-for line in zipfile.open(zipfile.namelist()[0]).readlines():
-    pass
-    # print(line.decode('utf-8'))
+for test_file in test_files:
+    file = open(test_file, 'rb')
+    zipfile = ZipFile(BytesIO(file.read()))
+    for line in zipfile.open(zipfile.namelist()[0]).readlines():
+        # Transformation and loading to db
+        # print(line.decode('utf-8'))
+        pass
 
 print('Execution time:', time.time() - start_time)
